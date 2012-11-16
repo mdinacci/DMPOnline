9b86792a2037db13a05031a2d6992b49fdcd7a12
Adrian Richardson <Adrian.Richardson@ed.ac.uk>
2012-11-16 17:41:15 +0000

    Share notification sent message improvements and button wording changes

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -27,6 +27,7 @@ class PlansController < ApplicationController
   # GET /plans/1
   def show
 #    @plan = Plan.for_user(current_user).find(params[:id])
+    session[:sent_notification] ||= []
   end
 
   # GET /plans/new
@@ -233,12 +234,14 @@ EOSQL
   # PUT /plan/1/notify/1
   def notify
 #    @plan = Plan.for_user(current_user).find(params[:id])
+    session[:sent_notification] ||= []
 
     @tir = @plan.template_instance_rights.find(params[:template_instance_right_id])
     unless @tir.blank? || @tir.email_mask =~ /[%*]/
       email = @tir.display_email_mask
       Notifier.plan_shared(email, current_user.email, @plan).deliver
       flash[:notice] = I18n.t('dmp.notify.sent', email: email)
+      session[:sent_notification][@tir.id] = true
     else
       flash[:warning] = I18n.t('dmp.notify.not_sent', email: email)
     end
